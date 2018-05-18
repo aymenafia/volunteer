f36cd95aaf9ffeab8ccb6f8cfabde726dc88ec21
Mohamed Aymen Afia <mohamedaymen.afia@esprit.tn>
2018-05-18 04:20:33 +0100

    implementer interface 1 du formulaire

--- Benevole/AppDelegate.swift
+++ Benevole/AppDelegate.swift
@@ -17,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UITabBar.appearance().tintColor = UIColor.black
+        
+        
         return true
     }
 
